e8140f0777f4b9ca64a174210bf489fa620dc8d7
Jose Gonzalez <joseraul88@gmail.com>
2022-09-07 11:31:24 -0500

    First commit

---
+++ disemvowel-trolls.js
@@ -0,0 +1,32 @@
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+  let noVowels = [];
+  const newStr = str.split("");
+  newStr.forEach((l) => {
+    if (
+      l !== "a" &&
+      l !== "e" &&
+      l !== "i" &&
+      l !== "o" &&
+      l !== "u" &&
+      l !== "A" &&
+      l !== "E" &&
+      l !== "I" &&
+      l !== "O" &&
+      l !== "U"
+    ) {
+      noVowels.push(l);
+    }
+  });
+
+  return noVowels.join("");
+}
